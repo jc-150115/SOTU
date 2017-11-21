fe643e6e6c14677d3421d56e71c539c2e042d7af
燎 石橋 <150461@jc-21.jp>
2017-11-21 16:15:17 +0900

    初回

--- SQLiteSample001/SQLiteSample001/App.xaml.cs
+++ SQLiteSample001/SQLiteSample001/App.xaml.cs
@@ -1,34 +1,106 @@
 ﻿using System;
+using Xamarin.Forms;
+using System.Text;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-
-using Xamarin.Forms;
 
 namespace SQLiteSample001
 {
-    public partial class App : Application
+    public partial class App : Application //partial付けた
     {
         public App()
         {
-            InitializeComponent();
-
-            MainPage = new SQLiteSample001.MainPage();
+            MainPage = new MyPage();
         }
+    }
 
-        protected override void OnStart()
+    class MyPage : ContentPage
+    {
+
+        readonly TodoRepository _db = new TodoRepository();
+
+        public MyPage()
         {
-            // Handle when your app starts
+
+            var listView = new ListView
+            {
+                ItemsSource = _db.GetItems(),
+                ItemTemplate = new DataTemplate(typeof(TextCell))
+            };
+
+            listView.ItemTemplate.SetBinding(TextCell.TextProperty, "Text");
+            //listView.ItemTemplate.SetBinding(TextCell.DetailProperty, new Binding("CreatedAt", stringFormat: "{0:yyy/MM/dd hh:mm}"));
+            listView.ItemTemplate.SetBinding(TextCell.DetailProperty, new Binding("CreatedAt", stringFormat: "{yyyy/mm/dd}"));
+            listView.ItemTemplate.SetBinding(TextCell.TextProperty, "Text");//画像のバイナリデータを入れたいけども
+
+            listView.ItemTapped += async (s, a) => {
+                var item = (TodoItem)a.Item;
+                if (await DisplayAlert("削除してい宜しいですか", item.Text, "OK", "キャンセル"))
+                {
+                    item.Delete = true; // 削除フラグを有効にして
+                    _db.SaveItem(item); // データベースの更新
+                    listView.ItemsSource = _db.GetItems(); // リスト更新
+                }
+            };
+
+            var entry = new Entry
+            {
+                HorizontalOptions = LayoutOptions.FillAndExpand
+            };
+
+            var entry2 = new Entry//
+            {
+                HorizontalOptions = LayoutOptions.FillAndExpand
+            };
+
+            var buttonAdd = new Button
+            {
+                WidthRequest = 60,
+                TextColor = Color.White,
+                Text = "Add"
+            };
+
+            var buttonAdd2 = new Button//
+            {
+                WidthRequest = 60,
+                TextColor = Color.White,
+                Text = "Add"
+            };
+
+            buttonAdd.Clicked += (s, a) => {
+                if (!String.IsNullOrEmpty(entry.Text))
+                { // Entryに文字列が入力されている場合に処理する
+                    var item = new TodoItem { Text = entry.Text, CreatedAt = DateTime.Now, Delete = false };
+                    _db.SaveItem(item);
+                    listView.ItemsSource = _db.GetItems(); //リスト更新
+                    entry.Text = ""; // 入力コントロールをクリアする
+                }
+            };
+
+            buttonAdd2.Clicked += (s, a) => {//追加分
+                if (!String.IsNullOrEmpty(entry.Text))
+                { // Entryに文字列が入力されている場合に処理する
+                    var item = new TodoItem { Text = entry.Text, CreatedAt = DateTime.Now, Delete = false };
+                    _db.SaveItem(item);
+                    listView.ItemsSource = _db.GetItems(); //リスト更新
+                    entry.Text = ""; // 入力コントロールをクリアする
+                }
+            };
+
+            Content = new StackLayout
+            {
+                Padding = new Thickness(0, Device.OnPlatform(20, 0, 0), 0, 0),
+                Children = {
+          new StackLayout {
+            BackgroundColor = Color.Red, // 入力部の背景色は赤で
+            Padding = 5,
+            Orientation = StackOrientation.Horizontal,
+            Children = {entry, buttonAdd,entry2,buttonAdd2} // Entryコントロールとボタンコントロールを横に並べる(余計な事したぁー)
+          },
+          listView // その下にリストボックスを置く
+        }
+            };
         }
 
-        protected override void OnSleep()
-        {
-            // Handle when your app sleeps
-        }
-
-        protected override void OnResume()
-        {
-            // Handle when your app resumes
-        }
     }
 }
---
+++ SQLiteSample001/SQLiteSample001/TodoRepository.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using SQLite.Net;
+using Xamarin.Forms;
+
+namespace SQLiteSample005
+{
+    class TodoRepository
+    {
+
+        static readonly object Locker = new object();
+        readonly SQLiteConnection _db;
+
+        public TodoRepository()
+        {
+            _db = DependencyService.Get<ISQLite>().GetConnection();
+            _db.CreateTable<TodoItem>();
+        }
+
+        public IEnumerable<TodoItem> GetItems()
+        {
+            lock (Locker)
+            {
+                // Delete==falseの一覧を取得する（削除フラグが立っているものは対象外）
+                return _db.Table<TodoItem>().Where(m => m.Delete == false); // <-7
+            }
+        }
+
+        public int SaveItem(TodoItem item)
+        {
+            lock (Locker)
+            {
+                if (item.Id != 0)
+                { // Idが0でない場合は、更新
+                    _db.Update(item);
+                    return item.Id;
+                }
+                return _db.Insert(item);
+            }
+        }
+    }
+}
